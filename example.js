d2ed075b39849cfdac3cb3c7acead28e7430d092
Matt McKegg <matt@wetsand.co.nz>
2014-07-30 19:49:11 +1200

    use loop-drop-audio-context in test

--- example.js
+++ example.js
@@ -3,7 +3,7 @@ var element = document.createElement('div')
 document.body.appendChild(element)
 
 // require audio context from loop drop
-var audioContext = require('../loop-drop-app/audio-context')
+var audioContext = require('loop-drop-audio-context')
 
 var remote = Remote(audioContext, element)
 remote.output.connect(audioContext.destination)
